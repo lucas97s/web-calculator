6d598ff9e4362d53876767cf258ff473a0cd07f0
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-04-21 16:12:03 +0800

    Modify and add functionality and design to the calc
    This commit is to ensure the progress is saved.

--- external.js
+++ external.js
@@ -18,18 +18,41 @@ const multiply = function(num1,num2) {
 return num1*num2 ; 
 };
 
+function resetVariables () {
+    firstNumber = '' ; 
+    secondNumber = '' ; 
+    operator = '';
+    clearOrFloat = '' ; 
+    index = 1 ; 
+    answer.textContent = '' ;
+    numberDisplay.textContent = '';
+    flipPercentage = 1 ; 
+}
+
 let firstNumber = '' ; 
 let secondNumber = '' ; 
 let operator = '';
 let clearOrFloat = '' ; 
 let index = 1 ; 
-let indexForNumber = 1 ; 
+const numberOfDecimalPlace = 2 ; 
+let flipPercentage = 1 ; 
+const minLengthFirstNumber = 7 ; 
+const maxTotalLength = 12 ; 
+let minLengthForOperator = 11 ; 
+
+
+console.log(secondNumber == '')   
 
 function operation (firstNumber,secondNumber, operator) {
     if (operator === "+") return add(firstNumber,secondNumber) ; 
-    if (operator === "-") return substract(firstNumber,secondNumber) ; 
+    if (operator === "-") return subtract(firstNumber,secondNumber) ; 
     if (operator === "/") return divide(firstNumber,secondNumber) ; 
     if (operator === "x") return multiply(firstNumber,secondNumber) ; 
+}
+
+function roundUpToDecimal (number, demicalPlace) {
+    let factor = Math.pow(10,demicalPlace); 
+    return Math.ceil(number*factor) / factor ; 
 }
 
 //gettin important nodes 
@@ -39,7 +62,6 @@ const answer = document.querySelector(".answer") ;
 const numberDisplay = document.querySelector(".numbers-display")
 
 
-
 //getting operators 
 
 const operatorsNode = document.querySelectorAll(".operators .child-buttons") ;
@@ -47,14 +69,23 @@ console.log(operatorsNode) ;
 
 operatorsNode.forEach(button => {
     button.addEventListener('click', function(element){
+        let lengthCheck = numberDisplay.textContent ;
+        console.log(lengthCheck.length) ; 
+
+        if (lengthCheck.length < maxTotalLength){
+            
  
-        if ( !(firstNumber === '') && operator == 0  ) { 
-            operator = element.target.textContent ;
-            console.log(operator) ; 
-            console.log(index) ;  
-            index = 2 ; 
-            numberDisplay.textContent = firstNumber + ' ' + operator ;
+            if ( !(firstNumber === '') && operator == 0  ) { 
+                operator = element.target.textContent ;
+                console.log(operator) ; 
+                console.log(index) ;  
+                index = 2 ; 
+                numberDisplay.textContent = firstNumber + ' ' + operator ;
+            }
+            
+
         }
+
         
     })
 });
@@ -69,20 +100,48 @@ console.log(numberNodes) ;
 numberNodes.forEach(button => {
     button.addEventListener('click', function(element){
 
-        if (index == 1){
-            firstNumber = firstNumber + element.target.textContent ; 
-            console.log (firstNumber) ;
-            numberDisplay.textContent = firstNumber ;
-        }
-    
+        let lengthCheck = numberDisplay.textContent ;
+        console.log(lengthCheck.length) ;  
 
-        if (index == 2) {
-            secondNumber = secondNumber + element.target.textContent ; 
-            console.log(secondNumber) ; 
-            numberDisplay.textContent = firstNumber + ' ' + operator + ' ' + secondNumber ;  
+        // After a dot has been acquired no longer dot can be initiated 
+        if( lengthCheck.length <= minLengthFirstNumber ){
+            
+            if (index == 1 ){
+                if (!(firstNumber.includes('.') && element.target.textContent == ".")) {
+                    firstNumber = firstNumber + element.target.textContent ; 
+                    console.log (firstNumber) ;
+                    numberDisplay.textContent = firstNumber ;
+                }
+
+            }
+        
+
+            if (index == 2 && !(operator == 0)) {
+                if (!(secondNumber.includes('.') && element.target.textContent == ".")){
+                    secondNumber = secondNumber + element.target.textContent ; 
+                    console.log(secondNumber) ; 
+                    numberDisplay.textContent = firstNumber + ' ' + operator + ' ' + secondNumber ;  
+                }
+
+            }
+
+        } else if ( (lengthCheck.length <= minLengthForOperator) && !(operator == 0) ){
+            if (index == 2 && !(operator == 0)) {
+                if (!(secondNumber.includes('.') && element.target.textContent == ".")){
+                    secondNumber = secondNumber + element.target.textContent ; 
+                    console.log("this is being triggered") ;
+                    console.log(operator) ;
+                    console.log(operator == 0) ; 
+                    numberDisplay.textContent = firstNumber + ' ' + operator + ' ' + secondNumber ;  
+                    minLengthForOperator = maxTotalLength ; 
+                    }
+
+
+            }
+
         }
-    })
-});
+    });
+}); 
 
 
 //getting other operators 
@@ -92,18 +151,43 @@ console.log(otherNodes) ;
 
 otherNodes.forEach(button => {
     button.addEventListener('click', function(element){
-        firstNumber = parseFloat(firstNumber) ; 
-        secondNumber = parseFloat(secondNumber) ;    
+
         clearOrFloat = element.target.textContent ;
-        if (!(secondNumber === '') && clearOrFloat == "=") {
-            firstNumber = operation(firstNumber,secondNumber,operator) ; 
+
+        if (!(secondNumber == '') && clearOrFloat == "=") {
+            firstNumber = parseFloat(firstNumber) ; 
+            secondNumber = parseFloat(secondNumber) ;    
+            firstNumber = roundUpToDecimal(operation(firstNumber,secondNumber,operator),numberOfDecimalPlace) ; 
             console.log(firstNumber) ;
             answer.textContent = firstNumber ; 
             secondNumber = ''   ; 
             operator = '' ; 
-            numberDisplay.textContent = firstNumber 
+            numberDisplay.textContent = firstNumber ;
 
         }
+
+        if (clearOrFloat == "AC") {
+            resetVariables() ; 
+        }
+
+        if (clearOrFloat == "%" && operator == '' && flipPercentage == 1) {
+            firstNumber = firstNumber/100 ; 
+            numberDisplay.textContent = firstNumber ;
+            answer.textContent = firstNumber ; 
+            flipPercentage = 2 ; 
+        } else if (clearOrFloat == "%" && operator == '' && flipPercentage == 2){
+            firstNumber = firstNumber*100 ; 
+            numberDisplay.textContent = firstNumber ;
+            answer.textContent = firstNumber ; 
+            flipPercentage = 1 ; 
+        }
+
+        
+        if (clearOrFloat == "+/-" && operator == '' ) {
+            firstNumber = firstNumber*(-1) ; 
+            numberDisplay.textContent = firstNumber ;
+            answer.textContent = firstNumber ;  
+        } 
 
     })
 });
@@ -111,5 +195,4 @@ otherNodes.forEach(button => {
 /* Okay I am stuck, now I know how to do the operation. What I want. 
 Firstly we need to click the first number, then move to the operator then only second number. This sequence must be followed all times.
 If they want to add more */
-
 
